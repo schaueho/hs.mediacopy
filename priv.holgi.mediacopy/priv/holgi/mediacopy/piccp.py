661deacc85e485129648e226226dd1b3f46a1b68
Holger Schauer <holger.schauer@gmx.de>
2010-11-16 17:46:24 +0100

    Handle new argument to walk over a tree of files. Preliminary support for exif parsing of files.

--- priv.holgi.mediacopy/priv/holgi/mediacopy/piccp.py
+++ priv.holgi.mediacopy/priv/holgi/mediacopy/piccp.py
@@ -1,37 +1,79 @@
 #!/usr/bin/python
 
-from os import getenv, access, path, X_OK, W_OK
-import datetime
+import os
+import EXIF as exif
+import shutil
+from stat import S_ISDIR, S_ISREG, ST_MODE
 from optparse import OptionParser
 
-destination = path.join(getenv("HOME"), "Bilder")
+_destination = os.path.join(os.getenv("HOME"), "Bilder", "new")
 
 def parse_options():
-    parser = OptionParser()
+    usage = "usage: %prog [options] souredir"
+    parser = OptionParser(usage=usage)
     parser.add_option('-v', '--verbose', dest="verbose", action="store_true",
                       help="verbose logging")
     parser.add_option('-n', '--noaction', dest="noaction", action="store_true",
                       help="don't perform the action")
     parser.add_option('-d', '--destination', dest="destination", 
-                      type="string", default=destination, 
-                      help="set destination (default: %s)" % destination)
+                      type="string", default=_destination, 
+                      help="set destination (default: %s)" % _destination)
     options, args = parser.parse_args()
+    if len(args) != 1:
+        parser.error("incorrect number of arguments")
     return [parser, options, args]
 
 def validate_destination(dest):
     "Validate that the destination exists and is writable."
-    if not(path.exists(dest) and path.isdir(dest) and 
-           access(dest, W_OK | X_OK)):
+    if not(os.path.exists(dest) and os.path.isdir(dest) and 
+           os.access(dest, os.W_OK | os.X_OK)):
         raise IOError("Destination %s doesn't exist or isn't writable" % dest)
     return True
 
+def walktree(top, callback):
+    '''recursively descend the directory tree rooted at top,
+       calling the callback function for each regular file'''
+
+    for f in os.listdir(top):
+        pathname = os.path.join(top, f)
+        mode = os.stat(pathname)[ST_MODE]
+        if S_ISDIR(mode):
+            # It's a directory, recurse into it
+            walktree(pathname, callback)
+        elif S_ISREG(mode):
+            # It's a file, call the callback function
+            callback(pathname)
+        else:
+            # Unknown file type, print a message
+            print 'Skipping %s' % pathname
+
+def parse_exif(file):
+    "Parse exif tag from file"
+    print 'visiting: %s' % file
+    f = open(file, 'rb')
+    tags = exif.process_file(f)
+    try:
+        print tags['EXIF DateTimeOriginal']
+    except KeyError:
+        print "Can't read exif date and time (original)"
+
+def copy_file(file, destination):
+    "Copy file to destination"
+    shutil.copy2(file, destination)
+
 def main():
+    callback = None
     parser, options, args = parse_options()
     try:
         validate_destination(options.destination)
     except IOError, e:
         parser.print_help()
         raise e
+    if options.noaction:
+        callback = visitfile
+    else:
+        callback = parse_exif
+    walktree(args[0], callback)
 
 if __name__ == "__main__":
     main()
