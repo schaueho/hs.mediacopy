2b866281b5cf6c1256a2b3bb85807155c2ebe1c3
Holger Schauer <holger.schauer@gmx.de>
2010-11-29 18:35:15 +0100

    Add option --force to overwrite.

--- priv.holgi.mediacopy/mediacp.py
+++ priv.holgi.mediacopy/mediacp.py
@@ -8,10 +8,12 @@ from priv.holgi.mediacopy.filelib import validate_destination, \
 _destination = os.path.join(os.getenv("HOME"), "Bilder", "Fotos")
 
 def parse_options():
-    usage = "usage: %prog [options] souredir"
+    usage = "usage: %prog [options] sourcedir"
     parser = OptionParser(usage=usage)
     parser.add_option('-v', '--verbose', dest="verbose", action="store_true",
                       help="verbose logging")
+    parser.add_option('-f', '--force', dest="force", action="store_true",
+                      help="force overwrite")
     parser.add_option('-n', '--noaction', dest="noaction", action="store_true",
                       help="don't perform the action")
     parser.add_option('-d', '--destination', dest="destination", 
@@ -33,7 +35,7 @@ def main():
     if options.noaction:
         callback = print_filename
     else:
-        callback = lambda f: copy_file(f, options.destination)
+        callback = lambda f: copy_file(f, options.destination, options.force)
     walktree(args[0], callback)
 
 if __name__ == "__main__":
